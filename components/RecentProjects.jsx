7c173cd4dd79e105fc95da798383f02f31c11038
DonaldJasper0621 <donaldsu910621@gmail.com>
2024-07-27 09:34:30 +0800

    first

---
+++ components/RecentProjects.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {projects} from '@/data'
+
+const RecentProjects = () => {
+  return (
+    <div className='py-20'>
+        <h1>
+            a small
+        </h1>
+    </div>
+  )
+}
+
+export default RecentProjects
