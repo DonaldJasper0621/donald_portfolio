a13362b01fbbc9a38b2e14f109ff60e4093a5211
Donald Jasper Su <donaldsu910621@gmail.com>
2024-09-17 22:28:50 -0400

    fix

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,31 +1,35 @@
 import { FaLocationArrow } from "react-icons/fa6";
 import { socialMedia } from "@/data";
 import MagicButton from "./ui/MagicButton";
+import Image from "next/image";
 
 const Footer = () => {
   return (
     <footer className="w-full pt-20 pb-10" id="contact">
       {/* background grid */}
       <div className="w-full absolute left-0 -bottom-72 min-h-96">
-        <img
+        <Image
           src="/footer-grid.svg"
           alt="grid"
+          layout="fill"
           className="w-full h-full opacity-50 "
         />
       </div>
 
       <div className="flex flex-col items-center">
         <h1 className="heading lg:max-w-[45vw]">
-          I'm ready to <span className="text-purple">Conquer</span> and{" "}
+          I&apos;m ready to <span className="text-purple">Conquer</span> and{" "}
           <span className="text-purple">Shine</span> in my future work.
         </h1>
         <p className="text-white-200 md:mt-10 my-5 text-center">
           Reach out to me today and let&apos;s discuss how I can help you
           achieve your goals.
         </p>
-        <a href="www.linkedin.com/in/donald-su-763963244" // Replace with your desired external link
-            target="_blank"
-            rel="noopener noreferrer"> 
+        <a
+          href="https://www.linkedin.com/in/donald-su-763963244"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <MagicButton
             title="Linked In"
             icon={<FaLocationArrow />}
@@ -44,7 +48,7 @@ const Footer = () => {
               key={info.id}
               className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
             >
-              <img src={info.img} alt="icons" width={20} height={20} />
+              <Image src={info.img} alt="icons" width={20} height={20} />
             </div>
           ))}
         </div>
