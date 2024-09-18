80798c4796f807612b6f98172250627102f98331
Donald Jasper Su <donaldsu910621@gmail.com>
2024-09-18 14:10:39 -0400

    change

--- components/ui/MagicButton.tsx
+++ components/ui/MagicButton.tsx
@@ -1,13 +1,5 @@
 import React from "react";
 
-/**
- *  UI: border magic from tailwind css btns
- *  Link: https://ui.aceternity.com/components/tailwindcss-buttons
- *
- *  change border radius to rounded-lg
- *  add margin of md:mt-10
- *  remove focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50
- */
 const MagicButton = ({
   title,
   icon,
@@ -23,15 +15,16 @@ const MagicButton = ({
 }) => {
   return (
     <button
-      className="relative inline-flex h-12 w-full md:w-60 md:mt-10 overflow-hidden rounded-lg p-[1px] focus:outline-none"
+      className="relative inline-flex h-12 w-auto md:w-60 md:mt-10 overflow-hidden rounded-lg p-[1px] focus:outline-none"
       onClick={handleClick}
     >
-      <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+      {/* Adjust the inset to avoid the overflow issue */}
+      <span className="absolute inset-[-150%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
 
-      {/* remove px-3 py-1, add px-5 gap-2 */}
+      {/* Adjust padding and ensure it's responsive */}
       <span
         className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg
-             bg-[#0077b5] px-7 text-lg font-bold text-white backdrop-blur-3xl gap-2 ${otherClasses}`}
+             bg-[#0077b5] px-5 py-2 text-lg font-bold text-white backdrop-blur-3xl gap-2 ${otherClasses}`}
       >
         {position === "left" && icon}
         {title}
